f70b9364429b8a3a8b1d82964b6ea46ad69adba2
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-07-10 03:10:01 +0530

    Fix: Mongoose Depreciation warning fix

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-
-
+mongoose.set('useCreateIndex', true);
 class Database {
 
     constructor() {
